6ad05854291703dfc3d525bfb310468ebc60ba4b
tomoki <tomo.kuro.3489@gmail.com>
2023-01-29 00:34:26 +0900

    Webサービス　DBと接続

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_28_131038) do
+ActiveRecord::Schema.define(version: 2023_01_28_150754) do
 
   create_table "tweets", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
